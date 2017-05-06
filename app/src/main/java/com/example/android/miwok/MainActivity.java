8687938d83b214325ea907dd0cb05f09cb15b92c
Akshay Yadav <yadakshay@gmail.com>
2017-05-06 15:15:03 +0530

    implimented fragments adapter

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,12 +15,13 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
+/**
+ * Displays a {@link ViewPager} where each page shows a different day of the week.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -29,58 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-        // Find the View that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
 
-        // Set a click listener on that View
-            numbers.setOnClickListener(new View.OnClickListener() {
-                // The code in this method will be executed when the numbers View is clicked on.
-                @Override
-                public void onClick(View view) {
-                    Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                    startActivity(numbersIntent);
-                }
-            });
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
 
-        TextView family = (TextView) findViewById(R.id.family);
-        // Set a click listener on that View
-        family.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(familyIntent);
-            }
-        });
-
-
-        TextView colors = (TextView) findViewById(R.id.colors);
-        // Set a click listener on that View
-        colors.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(colorsIntent);
-            }
-        });
-
-
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-        // Set a click listener on that View
-        phrases.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(phrasesIntent);
-            }
-        });
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
     }
-    public void openNumbersList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
-
 }
---
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.android.miwok;
+
+/**
+ * Created by Akshay on 06-05-2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1){
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        }else{
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
